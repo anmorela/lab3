f50477e32c8463eab3e16bab77638b840c8f5c90
Audrey Moreland <anmorela@ucsd.edu>
2020-01-19 21:00:49 -0800

    let's see if all this works

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions.\n"+
+			"Too many questions is the Chan disease.\n"+
+			"The best way is just to observe the noise of the world.\n"+
+			"The answer to your questions?\n"+
+			"Ask your own heart.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("a.thumbnail").click(projectClick);
+    function projectClick(e) {
+    	e.preventDefault();
+    	var containingProject = $(this).closest(".project");
+	    var description = $(containingProject).find(".project-description");
+	    if (description.length == 0) {
+	       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	    } else {
+	       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	    }
+	}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
